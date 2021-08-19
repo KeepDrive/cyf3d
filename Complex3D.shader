00dadb1a9c6fc43b04c25bd089f6c8e3ea9b365f
KeepDrive <89096890+KeepDrive@users.noreply.github.com>
2021-08-19 23:04:58 +0000

    v1.2
    -New "Complex3D" shader, that should allow you to manipulate all 4 individual Vertices and all 4 UV Points for the 3Dified sprites

---
+++ Complex3D.shader
@@ -0,0 +1,113 @@
+shader "CYF/Complex3D"
+{
+    Properties
+    {
+        _MainTex("Sprite Texture", 2D) = "white" {}
+
+        _objPos ("Object Position", Vector) = (0,0,0,0)
+
+        _StencilComp("Stencil Comparison", Float) = 8
+        _Stencil("Stencil ID", Float) = 0
+        _StencilOp("Stencil Operation", Float) = 0
+        _StencilWriteMask("Stencil Write Mask", Float) = 255
+        _StencilReadMask("Stencil Read Mask", Float) = 255
+
+        _ColorMask("Color Mask", Float) = 15
+
+        [Toggle(UNITY_UI_ALPHACLIP)] _UseUIAlphaClip("Use Alpha Clip", Float) = 0
+    }
+    SubShader
+    {
+        Tags
+        {
+            "Queue" = "Transparent"
+            "IgnoreProjector" = "True"
+            "RenderType" = "Transparent"
+            "PreviewType" = "Plane"
+            "CanUseSpriteAtlas" = "True"
+        }
+
+        Stencil
+        {
+            Ref[_Stencil]
+            Comp[_StencilComp]
+            Pass[_StencilOp]
+            ReadMask[_StencilReadMask]
+            WriteMask[_StencilWriteMask]
+        }
+
+        Cull Off
+        Lighting Off
+        ZTest LEqual
+        ZWrite On
+        Blend SrcAlpha OneMinusSrcAlpha
+        ColorMask[_ColorMask]
+
+        Pass
+        {
+        CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+            #pragma target 2.0
+
+            #include "UnityCG.cginc"
+            #include "UnityUI.cginc"
+
+            #pragma multi_compile __ UNITY_UI_CLIP_RECT
+            #pragma multi_compile __ UNITY_UI_ALPHACLIP
+
+            struct appdata
+            {
+                float4 vertex   : POSITION;
+                float4 color    : COLOR;
+                float2 uv : TEXCOORD0;
+                UNITY_VERTEX_INPUT_INSTANCE_ID
+            };
+
+            struct v2f
+            {
+                float4 vertex   : SV_POSITION;
+                fixed4 color    : COLOR;
+                float2 uv : TEXCOORD0;
+                float4 worldPosition : TEXCOORD1;
+                UNITY_VERTEX_OUTPUT_STEREO
+            };
+
+            sampler2D _MainTex;
+            float4 _objPos;
+            float4x4 mod,MVP;
+            fixed4 _TextureSampleAdd;
+            float4 _ClipRect;
+            float4 _MainTex_ST;
+            float4 vertPos[4],uvPos[4];//DX11 seems to not like this, but i guess we'll see if Unity is smart enough to compile them properly
+
+            v2f vert(appdata v)
+            {
+                v2f o;
+                UNITY_SETUP_INSTANCE_ID(v);
+                UNITY_INITIALIZE_VERTEX_OUTPUT_STEREO(o);
+                o.worldPosition = v.vertex;
+                float arrIndex=(v.uv.x*(1-v.uv.y))+2*(v.uv.x*v.uv.y)+3*((1-v.uv.x)*v.uv.y);//Branchless programming at its finest
+                o.uv = uvPos[arrIndex].xy;
+                o.vertex = mul(MVP,mul(mod,float4(vertPos[arrIndex].xyz,1))+_objPos);
+                o.color = v.color;
+                return o;
+            }
+            fixed4 frag(v2f i) : SV_Target
+            {
+                half4 color = (tex2D(_MainTex, i.uv) + _TextureSampleAdd) * i.color;
+
+                #ifdef UNITY_UI_CLIP_RECT
+                color.a *= UnityGet2DClipping(i.worldPosition.xy, _ClipRect);
+                #endif
+
+                #ifdef UNITY_UI_ALPHACLIP
+                clip(color.a - 0.001);
+                #endif
+
+                return color;
+            }
+        ENDCG
+        }
+    }
+}
--- cyf3d/Lua/Libraries/cyf3d.lua
+++ cyf3d/Lua/Libraries/cyf3d.lua
@@ -35,9 +35,9 @@ function cyf3dMatrixFlipZ(mat)
     endmatrix[4][3]=-endmatrix[4][3]
     return endmatrix
 end
-function cyf3dInArray(arr,obj)
-    for i = 1,#arr do
-        if arr[i][1]==obj then
+function cyf3dInArray(obj)
+    for i = 1,#cyf3dactiveObjects do
+        if cyf3dactiveObjects[i][1]==obj then
             return i
         end
     end
@@ -141,26 +141,38 @@ function cyf3dMVPMatrix()
     end
 end
 --Actual commands:
-function cyf3dAddShader(obj)
-    if pcall(obj.shader.Set,"cyf3d","Basic3D") then--Shader should be compatible with any device but just in case
-        cyf3dactiveObjects[#cyf3dactiveObjects+1]={obj,{0,0,0},{1,1,1},cyf3dScaleMatrix(),cyf3dScaleMatrix()}--Any non-persistent properties we want to apply every frame so we save the rotation and the scale and have two cached matrices, one for vertices the other for UV, for now simple identity matrices
+function cyf3dAddShader(obj,type)
+    type=type or "Basic3D"
+    if pcall(obj.shader.Set,"cyf3d",type) then--Basic3D and Complex3D Shaders should be compatible with any device but just in case
+        cyf3dactiveObjects[#cyf3dactiveObjects+1]={obj,{0,0,0},{1,1,1},cyf3dScaleMatrix(),cyf3dScaleMatrix(),0}--Any non-persistent properties we want to apply every frame so we save the rotation and the scale and have two cached matrices, one for vertices the other for UV, for now simple identity matrices
+        if type=="Complex3D" then
+            cyf3dactiveObjects[#cyf3dactiveObjects][3]={{0,0,0,0},{1,0,0,0},{1,1,0,0},{0,1,0,0}}
+            cyf3dactiveObjects[#cyf3dactiveObjects][6]=1
+            cyf3dactiveObjects[#cyf3dactiveObjects][7]={{-0.5,-0.5,0,0},{0.5,-0.5,0,0},{0.5,0.5,0,0},{-0.5,0.5,0,0}}
+        end
         obj.shader.SetWrapMode("repeat")--Make UVs work as intended
     else
-        DEBUG("cyf3d shader failed to load")
+        DEBUG("cyf3d "..type.." shader failed to load")
     end
 end
-function cyf3dAddShaderPosRotScale(obj,posVector,rotVector,scaleVector)
-    if pcall(obj.shader.Set,"cyf3d","Basic3D") then
-        cyf3dactiveObjects[#cyf3dactiveObjects+1]={obj,{0,0,0},{1,1,1},cyf3dScaleMatrix(),cyf3dScaleMatrix()}--Any non-persistent properties we want to apply every frame so we save the rotation and the scale and have two cached matrices, one for vertices the other for UV, for now simple identity matrices
+function cyf3dAddShaderPosRotScale(obj,posVector,rotVector,scaleVector,type)
+    type=type or "Basic3D"
+    if pcall(obj.shader.Set,"cyf3d",type) then
+        cyf3dactiveObjects[#cyf3dactiveObjects+1]={obj,{0,0,0},{1,1,1},cyf3dScaleMatrix(),cyf3dScaleMatrix(),0}
+        if type=="Complex3D" then
+            cyf3dactiveObjects[#cyf3dactiveObjects][3]={{0,0},{1,0},{1,1},{0,1}}
+            cyf3dactiveObjects[#cyf3dactiveObjects][6]=1
+            cyf3dactiveObjects[#cyf3dactiveObjects][7]={{-0.5,-0.5,0},{0.5,-0.5,0},{0.5,0.5,0},{-0.5,0.5,0}}
+        end
         obj.shader.SetWrapMode("repeat")
         cyf3dSetPos(obj,posVector)
         cyf3dSetRotAndScale(obj,rotVector,scaleVector)
     else
-        DEBUG("cyf3d shader failed to load")
+        DEBUG("cyf3d "..type.." shader failed to load")
     end
 end
 function cyf3dRemoveShader(obj)
-    table.remove(cyf3dactiveObjects,cyf3dInArray(cyf3dactiveObjects,obj))
+    table.remove(cyf3dactiveObjects,cyf3dInArray(obj))
     obj.shader.Revert()
 end
 function cyf3dUpdate()
@@ -171,8 +183,13 @@ function cyf3dUpdate()
             cyf3dactiveObjects[i][1].shader.SetMatrix("MVP",cachedMVP)
             modtable=cyf3dactiveObjects[i][4]
             cyf3dactiveObjects[i][1].shader.SetMatrix("mod",cyf3dactiveObjects[i][1].shader.matrix(modtable[1],modtable[2],modtable[3],modtable[4]))
-            modtable=cyf3dactiveObjects[i][5]
-            cyf3dactiveObjects[i][1].shader.SetMatrix("uvMod",cyf3dactiveObjects[i][1].shader.matrix(modtable[1],modtable[2],modtable[3],modtable[4]))
+            if cyf3dactiveObjects[i][6]==0 then
+                modtable=cyf3dactiveObjects[i][5]
+                cyf3dactiveObjects[i][1].shader.SetMatrix("uvMod",cyf3dactiveObjects[i][1].shader.matrix(modtable[1],modtable[2],modtable[3],modtable[4]))
+            else
+                cyf3dactiveObjects[i][1].shader.SetVectorArray("vertPos",cyf3dactiveObjects[i][7])
+                cyf3dactiveObjects[i][1].shader.SetVectorArray("uvPos",cyf3dactiveObjects[i][3])
+            end
         end
     end
 end
@@ -185,10 +202,10 @@ function cyf3dGetPos(obj)
     return curPos
 end
 function cyf3dGetRot(obj)
-    return cyf3dactiveObjects[cyf3dInArray(cyf3dactiveObjects,obj)][2]
+    return cyf3dactiveObjects[cyf3dInArray(obj)][2]
 end
 function cyf3dGetScale(obj)
-    return cyf3dactiveObjects[cyf3dInArray(cyf3dactiveObjects,obj)][3]
+    return cyf3dactiveObjects[cyf3dInArray(obj)][3]
 end
 function cyf3dSetPos(obj,posVector)
     curPos=cyf3dGetPos(obj)
@@ -202,7 +219,7 @@ function cyf3dSetPos(obj,posVector)
     obj.shader.SetVector("_objPos",posVector)
 end
 function cyf3dSetRot(obj,rotVector)
-    objIndex=cyf3dInArray(cyf3dactiveObjects,obj)
+    objIndex=cyf3dInArray(obj)
     objRot=cyf3dactiveObjects[objIndex][2]
     rotVector=rotVector or {}
     objRot[1]=rotVector[1] or objRot[1]
@@ -212,7 +229,7 @@ function cyf3dSetRot(obj,rotVector)
     cyf3dactiveObjects[objIndex][4]=cyf3dApplyMatrix(cyf3dRotationMatrix(objRot[1],objRot[2],objRot[3]),cyf3dScaleMatrix(objScale[1],objScale[2],objScale[3]))
 end
 function cyf3dSetScale(obj,scaleVector)
-    objIndex=cyf3dInArray(cyf3dactiveObjects,obj)
+    objIndex=cyf3dInArray(obj)
     objScale=cyf3dactiveObjects[objIndex][3]
     scaleVector=scaleVector or {}
     objScale[1]=scaleVector[1] or objScale[1]
@@ -222,7 +239,7 @@ function cyf3dSetScale(obj,scaleVector)
     cyf3dactiveObjects[objIndex][4]=cyf3dApplyMatrix(cyf3dRotationMatrix(objRot[1],objRot[2],objRot[3]),cyf3dScaleMatrix(objScale[1],objScale[2],objScale[3]))
 end
 function cyf3dSetRotScale(obj,rotVector,scaleVector)
-    objIndex=cyf3dInArray(cyf3dactiveObjects,obj)
+    objIndex=cyf3dInArray(obj)
     objRot=cyf3dactiveObjects[objIndex][2]
     rotVector=rotVector or {}
     objRot[1]=rotVector[1] or objRot[1]
@@ -239,20 +256,71 @@ function cyf3dSetPosRotScale(obj,posVector,rotVector,scaleVector)
     cyf3dSetPos(obj,posVector)
     cyf3dSetRotScale(obj,rotVector,scaleVector)
 end
+function cyf3dSetVertices(obj,verts)
+    objIndex=cyf3dInArray(obj)
+    if cyf3dactiveObjects[cyf3dInArray(obj)][6]==1 then
+        curVerts=cyf3dactiveObjects[objIndex][7]
+        local newVerts={}
+        for i=1,4 do
+            newVerts[i]={}
+            for j=1,3 do
+                if verts[5-i]==nil then
+                    newVerts[i][j]=curVerts[i][j]
+                else
+                    newVerts[i][j]=verts[5-i][j] or curVerts[i][j]
+                end
+            end
+            newVerts[i][4]=0
+        end
+        cyf3dactiveObjects[objIndex][7]=newVerts
+    else
+        DEBUG("Complex vertex modification system is for Complex3D only")
+    end
+end
 function cyf3dUVSetPos(obj,posVector)
-    posVector[1]=(posVector[1] or 0)+0.5
-    posVector[2]=(posVector[2] or 0)+0.5
-    posVector[3]=0
-    posVector[4]=0
-    obj.shader.SetVector("_uvPos",posVector)
+    if cyf3dactiveObjects[cyf3dInArray(obj)][6]==0 then
+        posVector[1]=(posVector[1] or 0)+0.5
+        posVector[2]=(posVector[2] or 0)+0.5
+        posVector[3]=0
+        posVector[4]=0
+        obj.shader.SetVector("_uvPos",posVector)
+    else
+        DEBUG("Simple UV modification system is for Basic3D shader only")
+    end
 end
 function cyf3dUVSetRotScale(obj,rotation,scaleVector)
-    objIndex=cyf3dInArray(cyf3dactiveObjects,obj)
-    cyf3dactiveObjects[objIndex][5]=cyf3dApplyMatrix(cyf3dRotationMatrix(rotation or 0),cyf3dScaleMatrix(scaleVector[1] or 1,scaleVector[2] or 1))
+    objIndex=cyf3dInArray(obj)
+    if cyf3dactiveObjects[objIndex][6]==0 then
+        cyf3dactiveObjects[objIndex][5]=cyf3dApplyMatrix(cyf3dRotationMatrix(rotation or 0),cyf3dScaleMatrix(scaleVector[1] or 1,scaleVector[2] or 1))
+    else
+        DEBUG("Simple UV modification system is for Basic3D shader only")
+    end
 end
 function cyf3dUVSetPosRotScale(obj,posVector,rotation,scaleVector)
     cyf3dUVSetPos(obj,posVector)
     cyf3dUVSetRotScale(obj,rotation,scaleVector)
+end
+function cyf3dUVSetPoints(obj,points)
+    objIndex=cyf3dInArray(obj)
+    if cyf3dactiveObjects[cyf3dInArray(obj)][6]==1 then
+        curPoints=cyf3dactiveObjects[objIndex][3]
+        local newPoints={}
+        for i=1,4 do
+            newPoints[i]={}
+            for j=1,2 do
+                if points[5-i]==nil then
+                    newPoints[i][j]=curPoints[i][j]
+                else
+                    newPoints[i][j]=points[5-i][j] or curPoints[i][j]
+                end
+            end
+            newPoints[i][3]=0
+            newPoints[i][4]=0
+        end
+        cyf3dactiveObjects[objIndex][3]=newPoints
+    else
+        DEBUG("Complex UV modification system is for Complex3D only")
+    end
 end
 function cyf3dSetCamPos(posVector)
     posVector=posVector or {}
