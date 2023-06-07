aa3be51260d7b75d2f8158444758ccfdc5cd7bcb
KeepDrive <ipad2kirillo@gmail.com>
2023-06-08 00:01:20 +0300

    Point add/sub use assertion for debug error

--- cyf3d/Lua/Libraries/cyf3d/point.lua
+++ cyf3d/Lua/Libraries/cyf3d/point.lua
@@ -3,32 +3,20 @@ Point.__index = Point
 
 function Point:__add(obj)
 --#DEBUG
-  local objType = type(obj)
-  if objType == "table" then
+  assert(type(obj) == "table", "Attempt to use point addition with unsupported object")
 --#DEBUGEND
-    self.x = self.x + (obj.x or obj[1])
-    self.y = self.y + (obj.y or obj[2])
-    self.z = self.z + (obj.z or obj[3])
---#DEBUG
-  else
-    error("Attempt to use point addition with unsupported object")
-  end
---#DEBUGEND
+  self.x = self.x + (obj.x or obj[1])
+  self.y = self.y + (obj.y or obj[2])
+  self.z = self.z + (obj.z or obj[3])
 end
 
 function Point:__sub(obj)
 --#DEBUG
-  local objType = type(obj)
-  if objType == "table" then
+  assert(type(obj) == "table", "Attempt to use point subtraction with unsupported object")
 --#DEBUGEND
-    self.x = self.x - (obj.x or obj[1])
-    self.y = self.y - (obj.y or obj[2])
-    self.z = self.z - (obj.z or obj[3])
---#DEBUG
-  else
-    error("Attempt to use point subtraction with unsupported object")
-  end
---#DEBUGEND
+  self.x = self.x - (obj.x or obj[1])
+  self.y = self.y - (obj.y or obj[2])
+  self.z = self.z - (obj.z or obj[3])
 end
 
 function Point:new(obj)
