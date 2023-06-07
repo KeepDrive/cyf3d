06bf4c2ae219482ff75d62c1b728fc5a23506339
KeepDrive <ipad2kirillo@gmail.com>
2023-06-08 00:03:05 +0300

    Point equality operator

--- cyf3d/Lua/Libraries/cyf3d/point.lua
+++ cyf3d/Lua/Libraries/cyf3d/point.lua
@@ -19,6 +19,16 @@ function Point:__sub(obj)
   self.z = self.z - (obj.z or obj[3])
 end
 
+function Point:__eq(obj)
+--#DEBUG
+  assert(type(obj) == "table", "Attempt to compare point with unsupported object")
+--#DEBUGEND
+  local equalsX = (self.x == (obj.x or obj[1]))
+  local equalsY = (self.y == (obj.y or obj[2]))
+  local equalsZ = (self.z == (obj.z or obj[3]))
+  return equalsX and equalsY and equalsZ
+end
+
 function Point:new(obj)
   obj = obj or {}
   setmetatable(obj, self)
