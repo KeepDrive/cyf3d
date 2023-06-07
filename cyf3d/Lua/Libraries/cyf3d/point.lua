8d3b1be1421de80f90b15b558cb39c38416c7ab9
KeepDrive <ipad2kirillo@gmail.com>
2023-06-07 23:50:43 +0300

    Point subtraction

--- cyf3d/Lua/Libraries/cyf3d/point.lua
+++ cyf3d/Lua/Libraries/cyf3d/point.lua
@@ -16,6 +16,21 @@ function Point:__add(obj)
 --#DEBUGEND
 end
 
+function Point:__sub(obj)
+--#DEBUG
+  local objType = type(obj)
+  if objType == "table" then
+--#DEBUGEND
+    self.x = self.x - (obj.x or obj[1])
+    self.y = self.y - (obj.y or obj[2])
+    self.z = self.z - (obj.z or obj[3])
+--#DEBUG
+  else
+    error("Attempt to combine point with unsupported object")
+  end
+--#DEBUGEND
+end
+
 function Point:new(obj)
   obj = obj or {}
   setmetatable(obj, self)
