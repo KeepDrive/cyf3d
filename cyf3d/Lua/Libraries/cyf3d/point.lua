7dcdfbec85a6f54876a0650cc09bc833b4bd654b
KeepDrive <ipad2kirillo@gmail.com>
2023-06-07 23:49:31 +0300

    Point addition

--- cyf3d/Lua/Libraries/cyf3d/point.lua
+++ cyf3d/Lua/Libraries/cyf3d/point.lua
@@ -1,6 +1,21 @@
 local Point = {x = 0, y = 0, z = 0}
 Point.__index = Point
 
+function Point:__add(obj)
+--#DEBUG
+  local objType = type(obj)
+  if objType == "table" then
+--#DEBUGEND
+    self.x = self.x + (obj.x or obj[1])
+    self.y = self.y + (obj.y or obj[2])
+    self.z = self.z + (obj.z or obj[3])
+--#DEBUG
+  else
+    error("Attempt to combine point with unsupported object")
+  end
+--#DEBUGEND
+end
+
 function Point:new(obj)
   obj = obj or {}
   setmetatable(obj, self)
