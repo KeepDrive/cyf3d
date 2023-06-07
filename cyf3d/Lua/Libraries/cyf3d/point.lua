08633df1a1246943b571a9dd23b08bec1b8dda82
KeepDrive <ipad2kirillo@gmail.com>
2023-06-08 00:42:33 +0300

    Point distance/length functions

--- cyf3d/Lua/Libraries/cyf3d/point.lua
+++ cyf3d/Lua/Libraries/cyf3d/point.lua
@@ -1,6 +1,8 @@
 local Point = {x = 0, y = 0, z = 0}
 Point.__index = Point
 
+local sqrt = math.sqrt
+
 function Point.__add(lhs, rhs)
 --#DEBUG
   assert(type(rhs) == "table", "Attempt to use point addition with unsupported object")
@@ -34,6 +36,17 @@ function Point:__mul(num)
   return new
 end
 
+function Point:__div(num)
+--#DEBUG
+  assert(type(num) == "number", "Attempt to divide point by non-number")
+--#DEBUGEND
+  local new = Point:new()
+  new.x = self.x * num
+  new.y = self.y * num
+  new.z = self.z * num
+  return new
+end
+
 function Point:__eq(obj)
 --#DEBUG
   assert(type(obj) == "table", "Attempt to compare point with unsupported object")
@@ -42,6 +55,28 @@ function Point:__eq(obj)
   local equalsY = (self.y == (obj.y or obj[2]))
   local equalsZ = (self.z == (obj.z or obj[3]))
   return equalsX and equalsY and equalsZ
+end
+
+function Point:squareVectorLength()
+  return self.x ^ 2 + self.y ^ 2 + self.z ^ 2
+end
+
+function Point:vectorLength()
+  return sqrt(self:squareVectorLength())
+end
+
+function Point:squareDistanceTo(obj)
+--#DEBUG
+  assert(type(obj) == "table", "Attempt to get distance to unsupported object")
+--#DEBUGEND
+  local xDist = (obj.x or obj[1]) - self.x
+  local yDist = (obj.y or obj[2]) - self.y
+  local zDist = (obj.z or obj[3]) - self.z
+  return xDist ^ 2 + yDist ^ 2 + zDist ^ 2
+end
+
+function Point:distanceTo(obj)
+  return sqrt(self:squareDistanceTo(obj))
 end
 
 function Point:new(obj)
