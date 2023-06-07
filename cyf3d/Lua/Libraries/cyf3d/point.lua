5496c0e5b3b4e948cfb750645ca891e8b5c2c16b
KeepDrive <ipad2kirillo@gmail.com>
2023-06-07 23:46:31 +0300

    Point constructor

--- cyf3d/Lua/Libraries/cyf3d/point.lua
+++ cyf3d/Lua/Libraries/cyf3d/point.lua
@@ -1,23 +1,6 @@
 local Point = {x = 0, y = 0, z = 0}
 Point.__index = Point
 
-function Point:__add(obj)
-  local objType = type(obj)
-  if objType == "number" then
-    self.x = self.x + obj
-    self.y = self.y + obj
-    self.z = self.z + obj
-  elseif objType == "table" then
-    self.x = self.x + (obj.x or obj[1])
-    self.y = self.y + (obj.y or obj[2])
-    self.z = self.z + (obj.z or obj[3])
---DEBUG
-  else
-    error("Attempt to combine point with unsupported object")
---DEBUGEND
-  end
-end
-
 function Point:new(obj)
   obj = obj or {}
   setmetatable(obj, self)
